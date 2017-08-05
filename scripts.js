93649e87253e1dd679ee1980536bea67d16f3bfc
Greg Izzy <izydorek.g@gmail.com>
2017-08-05 18:04:01 +0200

    add screen

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 //OOP - 11.4
 
-function Phone(brand, price, color) {
+function Phone(brand, price, color, screen) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
+  this.screen = screen;
 }
 
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + ", and screen have " + this.screen + ".");
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var GalaxyS6 = new Phone("Samsung", 2000, "black");
-var OnePlus = new Phone("One", 1550, "Gold");
+var iPhone6S = new Phone("Apple", 2250, "silver", "6 inches");
+var GalaxyS6 = new Phone("Samsung", 2000, "black", "5,5 inches");
+var OnePlus = new Phone("One", 1550, "Gold", "5 inches");
 
 //iPhone6S.printInfo();
 //GalaxyS6.printInfo();
